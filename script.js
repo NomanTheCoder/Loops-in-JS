4c0b84b24c52d4a6350bceedaa4e64218fbb4355
Kayani815 <nomannadeem815@gmail.com>
2024-03-12 00:18:21 +0500

    Loops in JS

---
+++ script.js
@@ -0,0 +1,33 @@
+// let n=4;
+// for(let i = 0; i<=15;i++){
+//     console.log(`${n} x ${i} = ${n*i}`);
+// }
+
+// let fname='Noman'
+// let lname='Nadeem'
+
+// for(i=0;i<=13;i++){
+//     console.log(`${fname + ' ' + lname}`);
+// }
+
+// let value = 3;
+// for (let i = 15; i >= 1; --i) {
+//     console.log(`${value} x ${i} = ${value * i}`);
+// }
+
+// let n=2;
+// let i=1
+// while(i<=10){
+    
+//     console.log(`${n*i}`);
+//     i++
+// }
+
+let n=10;
+ 
+do{
+console.log('Hello World');
+n++
+
+}
+while(n<8);
